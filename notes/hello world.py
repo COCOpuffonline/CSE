2b69c57a4f828d19d563fcb354205974e68e03e4
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2018-10-24 09:45:57 -0700

    Added our first notes

--- notes/hello world.py
+++ notes/hello world.py
@@ -26,3 +26,30 @@ print("Here is another one...")
 print(6 % 2)
 print(5 % 2)
 print(11 % 4)   # Modulus (Remainder)
+
+# Powers
+# What is 2^20
+print(2 ** 100)
+
+# Talking input
+name = input("What is your name?")
+print("Hello %s." % name)
+
+age = input("How old are you? > _")
+print("%s!? You belong in a museum." % age)
+print()
+print("%s is really old. They are %s years old." % (name, age))
+
+# Variable Assignments
+car_name = "Alexis Mobile"
+car_type = "Tesla"
+car_cylinders = 16
+car_miles_per_gallon = 0.01
+
+# Make it print "I have a car called Alexis Mobile. It is a Tesla."
+print("I have a car called %s. It is a %s" % (car_name, car_type))
+
+# Recasting
+real_age = int(input("How old are you again"))
+hidden_age = real_age + 5
+print("This is your real age: %d" % hidden_age)
