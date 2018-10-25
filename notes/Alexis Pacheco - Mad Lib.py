7df0ac3efe3b2ac0972e8a4a604ef1ef603493fd
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2018-10-25 09:42:20 -0700

    10/25/18

---
+++ notes/Alexis Pacheco - Mad Lib.py
@@ -0,0 +1,7 @@
+noun1 = input("Noun1=")
+print("The idea of getting rid of %s is absolutely crazy." % noun1)
+
+noun2 = input("Noun2=")
+noun3 = input("Noun3=")
+noun4 = input("Noun4=")
+print("After reading the articles, %s, by %s, it is obvious that %s are good for kids overall." % (Noun2=, Noun3=, Noun 4)
