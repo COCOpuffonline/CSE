69aa53f0f0dc888eef79aaadf58d43c5cc16be90
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2018-10-26 10:39:21 -0700

    10/26/18

--- notes/Alexis Pacheco - Mad Lib.py
+++ notes/Alexis Pacheco - Mad Lib.py
@@ -1,7 +1,15 @@
 noun1 = input("Noun1=")
-print("The idea of getting rid of %s is absolutely crazy." % noun1)
-
+verb1 = input("Verb1=")
 noun2 = input("Noun2=")
 noun3 = input("Noun3=")
-noun4 = input("Noun4=")
-print("After reading the articles, %s, by %s, it is obvious that %s are good for kids overall." % (Noun2=, Noun3=, Noun 4)
+name1 = input("Name1=")
+noun5 = input("Noun5=")
+verb2 = input("Verb2=")
+noun6 = input("Noun6=")
+adjective1 = input("Adjective1 that ends in es=")
+verb3 = input("Verb3=")
+
+print("The idea of getting rid of %s is absolutely %s." % (noun1, verb1))
+print("After reading the articles, %s, by %s, it is obvious that %s are good for kids overall." % (noun2, noun3, name1))
+print("%s help keep kids %s, engaged in %s, and teaches kids %s will help them be %s in life." % (noun5, verb2, noun6,
+                                                                                                  adjective1, verb3))
