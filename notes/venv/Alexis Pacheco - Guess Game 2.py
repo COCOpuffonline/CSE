f0c806e4e248c790a3d571ab8852f21447e1770c
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2018-11-02 09:45:19 -0700

    11/2/18

--- notes/venv/Alexis Pacheco - Guess Game 2.py
+++
@@ -1,3 +0,0 @@
-import random
-print("Guess a number form 1 to 10")
-Guess1 = input("Guess1=")
--- notes/venv/Alexis Pacheco - Guess Game.py
+++ notes/venv/Alexis Pacheco - Guess Game.py
@@ -1,9 +1,10 @@
 import random
+(random.randint(0, 10))
+
 print("Guess a number form 1 to 10")
 Guess1 = input("Guess1=")
 
-def Guess(Guess1):
-    if Guess1 == random.randint:
-        return "Correct!"
 
-    print(random.randint(0, 10))
+
+
+
