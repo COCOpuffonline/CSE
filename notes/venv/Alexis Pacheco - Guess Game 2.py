df5fc6367ff7e4e3dedfe7c23c0a3d48ec6ca730
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2018-11-07 09:45:33 -0800

    11/7/18

---
+++ notes/venv/Alexis Pacheco - Guess Game 2.py
@@ -0,0 +1,2 @@
+while guesesTaken < 6:
+    print('Guess a number from 1-10.')
--- notes/venv/Alexis Pacheco - Guess Game.py
+++ notes/venv/Alexis Pacheco - Guess Game.py
@@ -1,10 +1,18 @@
 import random
-(random.randint(0, 10))
+r = (random.randint(1, 10))
+guesses_left = 5
+playing = True
 
-print("Guess a number form 1 to 10")
-Guess1 = input("Guess1=")
+print("Guess a number from 1-10")
 
-
-
-
-
+while guesses_left > 0 and playing:
+    guess = int(input("Guess="))
+    if guess > r:
+        print ("Lower")
+        guesses_left -= 1
+    elif guess < r:
+        print ("Greater")
+        guesses_left -= 1
+    else:
+        print ("Correct!")
+        playing = False
