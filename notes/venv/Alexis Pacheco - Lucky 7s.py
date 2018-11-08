210f174243cd74e618fcf4fdbd9b4ae429477267
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2018-11-08 09:46:09 -0800

    11/8/18

--- notes/venv/Alexis Pacheco - Lucky 7s.py
+++ notes/venv/Alexis Pacheco - Lucky 7s.py
@@ -1,1 +1,17 @@
-cash = 15
+import random
+money = 15
+rounds = 0
+playing = True
+
+while money > 0 and playing:
+    money -= 1
+    rounds += 1
+
+    if random.randint (1, 6) + random.randint (1, 6) == 7:
+        money += 5
+    else:
+        playing = True
+        if money == 0:
+            print("Rounds played")
+            print(rounds)
+
