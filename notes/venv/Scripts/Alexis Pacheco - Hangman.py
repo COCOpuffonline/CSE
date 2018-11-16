89b3e3a58d9208ce6505a8bdee743e27a7e2d22e
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2018-11-16 09:44:49 -0800

    11/16/18

--- notes/venv/Scripts/Alexis Pacheco - Hangman.py
+++ notes/venv/Scripts/Alexis Pacheco - Hangman.py
@@ -1,1 +1,3 @@
-word = ('dog', 'fish', 'computer', 'mouse', 'chair', 'desktop', 'sweatshirt', 'vacation', 'calender', 'turtle')
+import random
+word = ('dog', 'fish', 'computer', 'mouse', 'chair', 'desktop', 'sweatshirt', 'vacation', 'calender', 'turtle'
+        'screen', 'planet')
