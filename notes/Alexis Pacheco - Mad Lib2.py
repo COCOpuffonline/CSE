61a99d105fa9680deaa40972f012bfe9ea4dbca6
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2018-11-28 09:46:51 -0800

    11/18/18

---
+++ notes/Alexis Pacheco - Mad Lib2.py
@@ -0,0 +1,16 @@
+word_list =
+noun1 = input("Noun1=")
+verb1 = input("Verb1 ending in ing=")
+noun2 = input("Noun2=")
+noun3 = input("Noun3=")
+name1 = input("Name1=")
+noun5 = input("Noun5=")
+verb2 = input("Verb2=")
+noun6 = input("Noun6=")
+adjective1 = input("Adjective1 that ends in es=")
+verb3 = input("Verb3=")
+
+print("The idea of getting rid of %s is absolutely %s." % (noun1, verb1))
+print("After reading the articles, %s, by %s, it is obvious that %s are good for kids overall." % (noun2, noun3, name1))
+print("%s help keep kids %s, engaged in %s, and teaches kids %s will help them be %s in life." % (noun5, verb2, noun6,
+                                                                                                  adjective1, verb3))
