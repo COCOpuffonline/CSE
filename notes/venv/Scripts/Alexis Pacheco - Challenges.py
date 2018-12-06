28734549ad25c5394a54efb6a75a7f3f72c1c97f
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2018-12-06 09:45:40 -0800

    12/6/18

--- notes/venv/Scripts/Alexis Pacheco - Challenges.py
+++ notes/venv/Scripts/Alexis Pacheco - Challenges.py
@@ -5,7 +5,7 @@ def challenge1(first_name, last_name):
 
 challenge1("Pacheco", "Alexis")
 
-# Challenge 2
+# Challenge 2 Not finished
 def challenge2(random_number):
     if random_number // 2:
         return "This number is an even number."
@@ -16,6 +16,15 @@ def challenge2(random_number):
 challenge2(19)
 
 # Challenge 3
-def chanllenge3(formula_triangle):
-    return ()
-    
+def challenge3(b, h):
+    return (b * h) / 2
+
+
+print(challenge3(20, 5))
+
+# Challenge 5
+def challenge5(r):
+    return (r * r) * 3.14
+
+
+print(challenge5(1.1))
