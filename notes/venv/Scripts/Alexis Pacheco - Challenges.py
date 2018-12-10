6eb1037bbb3792b33f1abcda44a8b6f821dea889
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2018-12-10 09:43:31 -0800

    12/10/18

--- notes/venv/Scripts/Alexis Pacheco - Challenges.py
+++ notes/venv/Scripts/Alexis Pacheco - Challenges.py
@@ -21,14 +21,14 @@ def challenge5(r):
 print(challenge5(1.1))
 
 # Challenge 6
-def challenge6(r):
-    return (4/3 * 3.14) * r ** 3
+def challenge6(x):
+    return (4/3 * 3.14) * x ** 3
 
 
 print(challenge6(3))
 
-# Challenge8
-def challenge8(n):
+# Challenge7
+def challenge7(n):
     return (n + n*n) + (n*n*n)
 
 print(challenge8(5))
