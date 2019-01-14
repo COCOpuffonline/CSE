cc0fb1b91152437833c586117dbd4a3c4e1019f8
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2019-01-14 09:13:57 -0800

    Resync Folders

--- Alexis Pacheco - Hangman 2.py
+++ Alexis Pacheco - Hangman 2.py
@@ -1,5 +1,13 @@
 import random
-word = ["dog", "mouse", "Volcano", "Computer!", "Concatenation", "slide", "desktop", "shoe", "sweater", "backpack"]
-random_word = random.choice(word)
-random.choice = '*' * len(word)
-print(random_word)
+guesses_left = 8
+playing = True
+
+print("I am thinking of a secret word, what is it?")
+
+word_list = ["dog", "mouse", "Volcano", "Computer!", "Concatenation", "slide", "desktop", "shoe", "sweater", "backpack"]
+
+wordset = set(word_list)
+print(random.sample(wordset, 1))
+
+random.sample = '*' * len()
+print(random.sample)
