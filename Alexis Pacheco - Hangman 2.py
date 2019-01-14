4d5d6a474bbb2edbe44481cb2a290e2277caddbe
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2019-01-14 09:45:26 -0800

    1/14/19

--- Alexis Pacheco - Hangman 2.py
+++ Alexis Pacheco - Hangman 2.py
@@ -2,12 +2,13 @@ import random
 guesses_left = 8
 playing = True
 
-print("I am thinking of a secret word, what is it?")
+print("I am thinking of a word, what is it?")
 
 word_list = ["dog", "mouse", "Volcano", "Computer!", "Concatenation", "slide", "desktop", "shoe", "sweater", "backpack"]
 
-wordset = set(word_list)
-print(random.sample(wordset, 1))
+randword = (random.choice(word_list))
 
-random.sample = '*' * len()
-print(random.sample)
+word_list = len(random.choice)
+print(word_list)
+
+# output = []
