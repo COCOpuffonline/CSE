095ed5f554d480239bccdbfa48f6e939ab15c76a
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2019-01-16 09:44:26 -0800

    1/16/19

--- Alexis Pacheco - Hangman 2.py
+++ Alexis Pacheco - Hangman 2.py
@@ -1,11 +1,16 @@
 import random
+guesses = 8
+playing = True
 
 print("I am thinking of a random word what is it?")
 
 words = ["dog", "mouse", "Volcano", "Computer!", "Concatenation", "slide", "desktop", "shoe", "sweater", "backpack"]
 
 randword = random.choice(words)
-print(random.choice(words))
-
+print(random.choice)
 random.choice = '*' * len(randword)
 print(random.choice)
+
+while guesses > 0:
+    guess = input("Guess a letter.")
+    if
