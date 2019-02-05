ef29768853d3e1b3c3db8a35e7cfb39e05c62141
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2019-02-05 09:44:09 -0800

    2/5/19

--- Alexis Pacheco - Adventure Game.py
+++ Alexis Pacheco - Adventure Game.py
@@ -1,27 +1,42 @@
 world_map = {
-    'R19A': {
-        'NAME': "Mr.Wiebe's Room",
-        'DESCRIPTION': "This is the classroom you are in right now. "
-                       "There are two doors on the the north wall",
+    'MAIN_ROOM': {
+        'NAME': "Room #1",
+        'DESCRIPTION': "You wake up in a very filthy room and you do not remember how you got there."
+                       "There is a couch in the middle of the room and a chimney that has not been used in a while.",
         'PATHS': {
-            'NORTH': "PARKING_LOT"
+            'EAST': "SECRET_ROOM"
         }
     },
-    'PARKING_LOT': {
-        'NAME': "This North Parking Lot",
-        'DESCRIPTION': "There are a couple cars picked here",
+    'SECRET_ROOM': {
+        'NAME': "Secret room",
+        'DESCRIPTION': "You are in a small room and notice a small table on your left and on your right"
+                       "you see a hole in the floor.",
         'PATHS': {
-            'SOUTH': 'R19A'
+            'WEST': 'MAIN_ROOM'
+        }
+    },
+    'HALLWAY_1': {
+        'NAME': "Hallway 2",
+        'DESCRIPTION': "To the north you see a door, on the south side there is another door, and to the west is a"
+                       "a room without a door.",
+        'PATHS': {
+            'NORTH': 'KITCHEN',
+            'SOUTH': 'HALLWAY_2',
+            'WEST': 'STORAGE_ROOM_1'
+        }
+    },
+    'KITCHEN': {
+        'NAME': 'Kitchen',
+        'DESCRIPTION': "In the kitchen you see a cabinet with two drawers on the south side."
+                       "To the north is a very dirty refrigerator."
+
 
         }
     }
 }
 
-# Controller
-# Nodes are the rooms
-# Nodes have name, description, and path
 playing = True
-current_node = world_map['R19A']
+current_node = world_map['MAIN_ROOM']
 directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'UP', 'DOWN']
 
 while playing:
