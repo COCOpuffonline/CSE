13ab249c362859921c9129e8ea4ace5eeab0d650
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2019-02-13 09:45:52 -0800

    2/13/19

--- notes/venv/Scripts/Alexis Pacheco - Object Notes.py
+++ notes/venv/Scripts/Alexis Pacheco - Object Notes.py
@@ -1,8 +1,8 @@
 class Phone (object):
-    def __init__ (self, carrier, charge_left):
+    def __init__(self, carrier, charge_left=50):  # This makes the object
         # These are attributes that a phone has.
         # These should all be relevant to out program
-        self.screen =  True
+        self.screen = True
         self.camera = 2
         self.microphone = True
         self.carrier = carrier
@@ -22,7 +22,7 @@ class Phone (object):
         if self.battery_left <= 0:
             print("You can't. The phone is dead.")
             return
-        self.battery_left -= duration * battery_loss per_minute
+        self.battery_left -= duration * battery_loss_per_minute
         if self.battery_left < 0:
             self.battery_left = 0
             print("Your phone dies during the conversation.")
@@ -30,10 +30,24 @@ class Phone (object):
             print("Your phone dies at the end of the conversation.")
         else:
             print("You successfully make the phone call.")
-            print("Your phone is now at %s" % self.battery_left)
+            print("Your phone is now at %s percent" % self.battery_left)
+
+    def smash_phone(self):
+        print("SMASH!!!!!!!!!!!!")
+        print("It broke.")
+        self.screen = False
 
 
+# Initialize Objects
 my_phone = Phone("ATT", 100)
 your_phone = Phone("Bell", 0)
+default_phone = Phone("Verizon")
 
+my_phone.make_call(60)
+my_phone.make_call(10)
+my_phone.charge(100)
+my_phone.make_call(10)
+your_phone.smash_phone()
+your_phone.make_call(1)
 
+print(Special_Random.RandomWiebe.my_random())
