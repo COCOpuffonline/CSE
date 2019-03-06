cc3c8511848b3f254e2afe70acf1b2e4717a9173
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2019-03-06 09:44:43 -0800

    3/6/19

--- notes/venv/Scripts/Alexis Pacheco - Items.py
+++ notes/venv/Scripts/Alexis Pacheco - Items.py
@@ -7,17 +7,37 @@ class Weapon(Item):
     def __init__(self, name, damage, durability):
         super(Weapon, self).__init__(name)
         self.damage = damage
-        self.durability = 100
+        self.durability = durability
 
-    def swing_weapon(self):
+    def use_weapon(self):
         self.durability -= 5
-        print("You swing your weapon with great force and damage it in return.")
+        print("You use your weapon and its durability drops.")
 
 
 class Knife(Weapon):
     def __init__(self):                   # Dam. Dura.
-        super(Knife, self).__init__("Knife", 4, 76)
+        super(Knife, self).__init__("Knife", 7, 76)
+
 
 class Browning_Hi_point(Weapon):
     def __init__(self):
-        super(Browning_Hi_point, self).__init__("Browning_Hi_point", 13, 100)
+        super(Browning_Hi_point, self).__init__("Browning_Hi_point", 21, 97)
+
+
+class rusty_Scissors(Weapon):
+    def __init__(self):
+        super(rusty_Scissors, self).__init__("rusty_Scissors", 3, 32)
+
+class P90(Weapon):
+    def __init__(self):
+        super(P90, self).__init__("P90", 29, 147)
+
+# Work on this later
+class Potion(Item):
+    def __init__(self, name, health_healed):
+        super(Potion, self).__init__(name)
+        self.health_healed = health_healed
+
+    def drink_potion(self):
+        self.health_healed = h
+
