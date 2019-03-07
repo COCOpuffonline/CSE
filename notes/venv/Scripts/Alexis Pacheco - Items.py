a30433d2e4f5b6d79979e13ffd8adb4f59af16fc
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2019-03-07 09:44:13 -0800

    3/7/19

--- notes/venv/Scripts/Alexis Pacheco - Items.py
+++ notes/venv/Scripts/Alexis Pacheco - Items.py
@@ -23,14 +23,40 @@ class Browning_Hi_point(Weapon):
     def __init__(self):
         super(Browning_Hi_point, self).__init__("Browning_Hi_point", 21, 97)
 
+    def shoot(self):
+        self.durability -= 3
+        print("You shoot your pistol.")
+
 
 class rusty_Scissors(Weapon):
     def __init__(self):
         super(rusty_Scissors, self).__init__("rusty_Scissors", 3, 32)
 
-class P90(Weapon):
+
+class P90_B_Shot(Weapon):
     def __init__(self):
-        super(P90, self).__init__("P90", 29, 147)
+        super(P90_B_Shot, self).__init__("P90_B_Shot", 29, 147)
+
+    def shoot(self):
+        self.durability -= 4
+        print("Your guns durability went down.")
+
+
+class Armor(Item):
+    def __init__(self, name, defence, equip, unequip):
+        super(Armor, self).__init__(name)
+        self.defence = defence
+        self.equip = False
+        self.unequip = False
+
+    def equip(self):
+        self.equip = True
+        print("You equip the armor which increases your defence.")
+
+    def unequip(self):
+        self.unequip = True
+        print("You unequip your armor.")
+
 
 # Work on this later
 class Potion(Item):
