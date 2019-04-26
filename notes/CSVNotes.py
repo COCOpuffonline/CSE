db012a93fc333b3d43c866d766504d42cac512a2
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2019-04-26 09:48:16 -0700

    4/26/19

--- notes/CSVNotes.py
+++ notes/CSVNotes.py
@@ -1,18 +1,47 @@
 import csv
 
+
+def validate(num: str):
+    if all_16_digits(num):
+        return False
+    if divisilbe_by_2(num) and divisilbe_by_3(num):
+        return True
+    return False
+
+def divisilbe_by_3(num: str):
+    first_num = int(num[0])
+    if first_num % 3 == 0:
+        return True
+    return False
+
+def divisilbe_by_2(num: str):
+    first_num = int(num[0])
+    if first_num % 2 == 0:
+        return True
+    return False
+
+def all_16_digits(num: str):
+    if len(num) == 16:
+        return True
+    else:
+        print("NOT EVERY NUMBER IS 16 DIGITS!!!!")
+
+
+
 # with open("Book1.csv", 'r') as old_csv:
-#    with open("MyNewFile.csv", 'w', newline='') as new_csv:
-#        print("Writing File...   ")
-#       reader = csv.reader(old_csv)
-#        writer = csv.writer(new_csv)
-#        for row in reader:
-#            old_number = int(row[0])
-#            new_number = old_number + 1
-#           row[0] = new_number
-#           writer.writerow(row)
-#           # print_int(old_number) + 1)
-            # print(old_number)
-print("OK")
+#     with open("MyNewFile.csv", 'w', newline='') as new_csv:
+#         print("Writing File...   ")
+#         reader = csv.reader(old_csv)
+#         writer = csv.writer(new_csv)
+#         for row in reader:
+#             old_number = row[0]  # This is a string
+#             first_num = int(old_number[0])  # This is the first
+#             if first_num % 2 == 0:
+#                 writer.writerow(row)
+#             # print_int(old_number) + 1)
+#             # print(old_number)
+# print("OK")
+
 
 with open("Book1.csv", 'r') as old_csv:
     with open("MyNewFile.csv", 'w', newline='') as new_csv:
@@ -21,9 +50,6 @@ with open("Book1.csv", 'r') as old_csv:
         writer = csv.writer(new_csv)
         for row in reader:
             old_number = row[0]  # This is a string
-            first_num = int(old_number[0])  # This is the first
-            if first_num % 2 == 0:
+            if validate(old_number):
                 writer.writerow(row)
-            # print_int(old_number) + 1)
-            # print(old_number)
-print("OK")
+print("Done")
