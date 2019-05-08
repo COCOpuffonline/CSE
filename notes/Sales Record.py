365e78918062791fbb388f283a0188d8fc7ec893
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2019-05-08 09:46:55 -0700

    5/8/19

--- notes/Sales Record.py
+++ notes/Sales Record.py
@@ -2,14 +2,10 @@ import csv
 
 
 with open("Sales Records.csv", 'r') as old_csv:
-    with open("MyNewFile.csv", 'w', newline='') as new_csv:
-        print("Writing File...   ")
-        reader = csv.reader(old_csv)
-        writer = csv.writer(new_csv)
-        for row in reader:
-            old_number = row[0]  # This is a string
-            first_num = int(old_number[0])  # This is the first
-            if first_num % 2 == 0:
-                writer.writerow(row)
-            print_int(old_number) + 1)
-            print(old_number)
+    print("Writing File...   ")
+    reader = csv.reader(old_csv)
+    for row in reader:
+        row = row[13]
+        print(row)
+
+# with open("MyNewFile.csv", 'w', newline='') as new_csv:
