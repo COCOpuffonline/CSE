d0e453db7824a940f42b6944d599a2d8a4f1cc10
Alexis <42385508+COCOpuffonline@users.noreply.github.com>
2019-05-15 09:47:06 -0700

    5/15/19

--- notes/Sales Record.py
+++ notes/Sales Record.py
@@ -22,51 +22,51 @@ with open("Sales Records.csv", 'r') as old_csv:
 
     for row in reader:
         if row[2] == "Cereal":
-            div += 1
-            Av1 = cereal / div
             cereal += float(row[13])
         if row[2] == "Baby Food":
-            div += 1
-            Av2 = baby_food / div
             baby_food += float(row[13])
         if row[2] == "Vegetables":
-            div += 1
-            Av3 = vegetables / div
             vegetables += float(row[13])
         if row[2] == "Household":
-            div += 1
-            Av4 = household / div
             household += float(row[13])
         if row[2] == "Personal Care":
-            div += 1
-            Av5 = personal_care / div
             personal_care += float(row[13])
         if row[2] == "Snacks":
-            div += 1
-            Av6 = snacks / div
             snacks += float(row[13])
         if row[2] == "Cosmetics":
-            div += 1
-            Av7 = cosmetics / div
             cosmetics += float(row[13])
         if row[2] == "Office Supplies":
-            div += 1
-            Av8 = office_supplies / div
             office_supplies += float(row[13])
         if row[2] == "Meat":
-            div += 1
-            Av9 = meat / div
             meat += float(row[13])
         if row[2] == "Fruits":
-            div += 1
-            Av10 = fruits / div
             fruits += float(row[13])
         if row[2] == "Clothes":
-            div += 1
-            Av11 = clothes / div
             clothes += float(row[13])
         if row[2] == "Beverages":
-            div += 1
-            Av12 = beverages / div
             beverages += float(row[13])
-print("Cereal = %s" % Av1)
+
+
+def max_num_in_list(list):
+    max = list[0]
+    for a in list:
+        if a > max:
+            max = a
+    return max
+
+
+print(max_num_in_list(("%s, %s") % cereal , meat))
+
+
+# print("Cereal profit = %s" % cereal)
+# print("Baby food profit = %s" % baby_food)
+# print("Vegetables profit = %s" % vegetables)
+# print("Household profit = %s" % household)
+# print("Personal care profit = %s" % personal_care)
+# print("Snacks profit = %s" % snacks)
+# print("Cosmetics profit = %s" % cosmetics)
+# print("Office supplies profit = %s" % office_supplies)
+# print("Meat profit = %s" % meat)
+# print("Fruit profit = %s" % fruits)
+# print("Clothes profit = %s" % clothes)
+# print("Beverages profit = %s" % beverages)
